68b425b16d4b88da6abbcd3de69ff3b934f5f40c
Rishu Jain <77292616+codewithrishu@users.noreply.github.com>
2022-03-18 14:05:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  let [message, setMessage] = useState("");
+  let [firstPassword, setFirstPassword] = useState("");
+  let [scndPassword, setScndPassword] = useState("");
+  let [color, setColor] = useState();
+  
+  
+
+
+  let passwordOne = (e) =>{
+    firstPassword = e.target.value;
+  }
+  let passwordTwo = (e) =>{
+    scndPassword = e.target.value;
+  }
+  let clickHandler = () => {
+    if(firstPassword === scndPassword){
+      setMessage("success");
+      setColor("green")
+    }else{
+      setMessage("password does not match");
+      setColor("red")
+    }
+    
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <input onChange = {passwordOne} type="password"></input>
+      <input onChange = {passwordTwo} type="password"></input>
+      <button onClick={clickHandler} >submit</button> 
+      <div>
+      <small style={{borderColor: color}}>{message}</small>
+      </div>
+       
     </div>
   );
 }
